6c6882642116be727450718d1f8573d8ac15b3b9
Adyasha Panigrahi <adyasha.panigrahi@nagarro.com>
2026-08-26 11:33:19 +0530

    bom changes

---
+++ src/test/java/com/testNGPractice/TestBom.java
@@ -0,0 +1,12 @@
+package com.testNGPractice;
+
+import org.testng.annotations.Test;
+
+public class TestBom {
+
+    @Test
+    public void bomTest(){
+        System.out.println("Automation: Selenium");
+
+    }
+}
